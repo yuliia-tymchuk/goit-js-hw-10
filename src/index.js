f419acffbb898f7d426c389d0ada21e358fefb10
lia2307 <juliatymchuk23@gmail.com>
2022-10-07 22:40:24 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+refs.searchBox.addEventListener('input', debounce(onSearchBox, DEBOUNCE_DELAY));
+
+function onSearchBox(event) {
+  const name = event.target.value.trim();
+  if (!name) {
+    return;
+  }
+
+  fetchCountries(name)
+    .then(data => {
+      createMarkup(data);
+    })
+    .catch(error => {
+      console.log(error);
+      Notify.failure(
+        'Too many matches found. Please enter a more specific name.',
+        { position: 'center-center', width: '400px', fontSize: '20px' }
+      );
+      clearMarkup();
+    });
+}
