a1d90c24e4bd1312be07620e278cd0c6f04bca8a
lia2307 <juliatymchuk23@gmail.com>
2022-10-08 00:51:03 +0300

    updated task

--- src/index.js
+++ src/index.js
@@ -26,10 +26,61 @@ function onSearchBox(event) {
     })
     .catch(error => {
       console.log(error);
-      Notify.failure(
-        'Too many matches found. Please enter a more specific name.',
-        { position: 'center-center', width: '400px', fontSize: '20px' }
-      );
+      Notify.failure('Oops, there is no country with that name', {
+        position: 'center-center',
+        width: '400px',
+        fontSize: '20px',
+      });
       clearMarkup();
     });
+}
+
+function createMarkup(data) {
+  if (data.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name', {
+      position: 'center-center',
+      width: '400px',
+      fontSize: '20px',
+    });
+    clearMarkup();
+    return;
+  }
+  if (data.length > 1 && data.length <= 10) {
+    refs.countryInfo.innerHTML = '';
+
+    const markupCountryList = data.map(({ name, flags }) => {
+      return `<li class="counry-item">
+      <img src="${flags.svg}" alt="country flag" width="60px" height="40px">
+      <h3 class="country">${name.official}</h3>
+    </li>`;
+    });
+    refs.countryList.innerHTML = markupCountryList.join('');
+    return;
+  }
+  if ((data.length = 1)) {
+    refs.countryList.innerHTML = '';
+
+    const { name, capital, population, flags, languages } = data[0];
+    const languagesNormalize = Object.values(languages).join(',');
+
+    const markupCountryInfo = `<div class="wrapper-name-country">
+    <img src="${flags.svg}" alt="country flag" width="60px" height="40px" />
+    <h2 class="country">${name.official}</h2>
+  </div>
+  <ul class="info-list">
+    <li class="info-item">Capital:&nbsp<span>${capital[0]}</span></li>
+    <li class="info-item">Population:&nbsp<span>${population}</span></li>
+    <li class="info-item">
+      Languages:&nbsp<span>${languagesNormalize}</span>
+    </li>
+  </ul>`;
+
+    refs.countryInfo.innerHTML = markupCountryInfo;
+    return;
+  }
+}
+
+function clearMarkup() {
+  refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
